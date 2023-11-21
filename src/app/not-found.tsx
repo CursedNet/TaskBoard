17a4baed23c7c5cfb8c98689392b0df4fc8b1d72
Devollo <122895078+Devollox@users.noreply.github.com>
2023-11-21 18:05:44 +0300

    Create not-found.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+export const metadata: Metadata = {
+    title: 'Not Found',
+    description: 'Not Found',
+}
+export default function NotFound() {
+    return (
+        <main>
+            
+        </main>
+    )
+}
